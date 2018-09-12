a33544c6a90717e701319d7c700842934f7ca584
NaorZakk <naor@jenovice.com>
2018-09-12 12:17:27 +0300

    Travis config

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     "964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com",
   googleClientSecret: "KnH-rZC23z4fr2CN4ISK4srN",
-  mongoURI: "mongodb://naor:password1@ds161411.mlab.com:61411/blog_dev",
+  mongoURI: "mongodb://127.0.0.1:27017/blog_ci",
   cookieKey: "123123123",
   redisUrl: "redis://127.0.0.1:6379"
 };
